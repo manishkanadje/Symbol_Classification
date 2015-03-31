d5fdabfb2a42d9b9611347c256e30ecf0a15ca39
Manish Kanadje <Manish@manishs-mbp.wireless.rit.edu>
2015-03-31 16:15:03 -0400

    remove duplicates and single point exclusion

--- features.py
+++ features.py
@@ -19,42 +19,54 @@ def featureExtraction(f):
     
     coordinates = nl.readFile(csv_file) 
     symbolList, labelList = getStrokeIds(lg_file)
-    #symbolList = [['0'], ['1'], ['2', '3'], ['4']]
-    #labelList = ['q', 'h', '+', 'c']
-    #print(symbolList)
-    #print(labelList)
-    for i in range(len(symbolList)):
-        symbol = symbolList[i]
-        resampleCoordinates = nl.resampleSymbol(coordinates, symbol)
-        normalCoordinates = nl.widthNormalizeStroke(resampleCoordinates, symbol)
-        #pdb.set_trace()
-        for key in normalCoordinates.keys():
-            # Add normalized distance as feature
-            normalCoordinates[key] = \
-                calculateStrokeEdgeDistance(normalCoordinates[key])
-            # Add curvature angle as feature
-            normalCoordinates[key] = \
-              getCurvatureFeature(normalCoordinates[key])
+
+    #pdb.set_trace()
+    for key in coordinates:
+        coordinates[key] = nl.duplicatePointRemoval(coordinates[key])
+
+        for i in range(len(symbolList)):
+            symbol = symbolList[i]
+            if (validSymbol(symbol, coordinates)):
+                #pdb.set_trace()
+                resampleCoordinates = nl.resampleSymbol(coordinates, symbol)
+                normalCoordinates = nl.widthNormalizeStroke(resampleCoordinates, symbol)
+                #pdb.set_trace()
+                for key in normalCoordinates.keys():
+                    # Add normalized distance as feature
+                    normalCoordinates[key] = \
+                        calculateStrokeEdgeDistance(normalCoordinates[key])
+                    # Add curvature angle as feature
+                    normalCoordinates[key] = \
+                    getCurvatureFeature(normalCoordinates[key])
             
-        #pdb.set_trace()
-        pointList = []
-        keyList = list(normalCoordinates.keys())
-        keyList.sort(key = int)
-        for j in range(len(keyList)):
-            key = keyList[j]
-            for k in range(len(normalCoordinates[key])):
-                pointList += normalCoordinates[key][k]
-        #pdb.set_trace()
-        featureList = createAllFeatures(pointList)
-        featureData.append(featureList)
-        labelData.append(labelList[i])
-    
+                #pdb.set_trace()
+                pointList = []
+                keyList = list(normalCoordinates.keys())
+                keyList.sort(key = int)
+                for j in range(len(keyList)):
+                    key = keyList[j]
+                    for k in range(len(normalCoordinates[key])):
+                        pointList += normalCoordinates[key][k]
+                #pdb.set_trace()
+                featureList = createAllFeatures(pointList)
+                featureData.append(featureList)
+                #pdb.set_trace()
+                labelData.append(labelList[i])
     #pdb.set_trace()
     # Delete this
     #print (featureData)
     #print (labelData)
     return featureData, labelData
     
+def validSymbol(symbol, coordinates):
+    flag = False
+    #pdb.set_trace()
+    for point in symbol:
+        if (len(coordinates[point]) > 1):
+            flag = True
+    return flag
+
+
 def getStrokeIds(fileName):
     symbolList, labelList = [], []
     inputFile = open(fileName, 'rU', newline = '\n')
@@ -67,7 +79,7 @@ def getStrokeIds(fileName):
     return symbolList, labelList
 
 def createAllFeatures(pointList):
-    print ("Fix this to create all the features for data points")
+    #print ("Fix this to create all the features for data points")
     return pointList
 
 def calculateStrokeEdgeDistance(stroke):
@@ -79,35 +91,44 @@ def calculateStrokeEdgeDistance(stroke):
         numerator = abs(nl.eucledianDist(point, firstPoint) - nl.eucledianDist \
                          (point, lastPoint))
         denominator = abs(nl.eucledianDist(firstPoint, lastPoint))
-        normalizedDist = 1 - (numerator / denominator)
+        # Considers the case where stroke length is 0
+        if (denominator != 0):
+            normalizedDist = 1 - (numerator / denominator)
+        else:
+            normalizedDist = 0
         point.append(normalizedDist)
         updatedStroke.append(point)
     return updatedStroke
 
 def getCurvatureFeature(stroke):
-    # Point 1, 2, 3
-    point1 = [stroke[0][0], stroke[0][1]]
-    point2 = [stroke[1][0], stroke[1][1]]
-    point3 = [stroke[2][0], stroke[2][1]]
+    # For stroke haveing length greater than 3
+    if (len(stroke) >= 3):
+        # Point 1, 2, 3
+        point1 = [stroke[0][0], stroke[0][1]]
+        point2 = [stroke[1][0], stroke[1][1]]
+        point3 = [stroke[2][0], stroke[2][1]]
 
-    #stroke[0].append(calculateCurvatureAngle(point1, point1, point2))
-    stroke[0].append(0)
-    stroke[1].append(calculateCurvatureAngle(point1, point2, point3))
-    for i in range(2, len(stroke) - 2):
-        prevPoint = [stroke[i - 2][0], stroke[i - 2][1]]
-        point = [stroke[i][0], stroke[i][1]]
-        nextPoint = [stroke[i + 2][0], stroke[i + 2][1]]
-        angle = calculateCurvatureAngle(prevPoint, point, nextPoint)
-        stroke[i].append(angle)
-    # Last 3 points
-    index = len(stroke) - 3
-    point1 = [stroke[index][0], stroke[index][1]]
-    point2 = [stroke[index + 1][0], stroke[index + 1][1]]
-    point3 = [stroke[index + 2][0], stroke[index + 2][1]]
+        #stroke[0].append(calculateCurvatureAngle(point1, point1, point2))
+        stroke[0].append(0)
+        stroke[1].append(calculateCurvatureAngle(point1, point2, point3))
+        for i in range(2, len(stroke) - 2):
+            prevPoint = [stroke[i - 2][0], stroke[i - 2][1]]
+            point = [stroke[i][0], stroke[i][1]]
+            nextPoint = [stroke[i + 2][0], stroke[i + 2][1]]
+            angle = calculateCurvatureAngle(prevPoint, point, nextPoint)
+            stroke[i].append(angle)
+        # Last 3 points
+        index = len(stroke) - 3
+        point1 = [stroke[index][0], stroke[index][1]]
+        point2 = [stroke[index + 1][0], stroke[index + 1][1]]
+        point3 = [stroke[index + 2][0], stroke[index + 2][1]]
     
-    stroke[index + 1].append(calculateCurvatureAngle(point1, point2, point3))
-    #stroke[index + 2].append(calculateCurvatureAngle(point2, point3, point3))
-    stroke[index + 2].append(0.0)
+        stroke[index + 1].append(calculateCurvatureAngle(point1, point2, point3))
+        #stroke[index + 2].append(calculateCurvatureAngle(point2, point3, point3))
+        stroke[index + 2].append(0.0)
+    else:
+        for point in stroke:
+            point.append(0.0)
     return stroke
 
 
@@ -129,25 +150,29 @@ def fileCall():
     # For a specific folder
     # trainingPaths = [trainingFolderPath + "MfrDB/"]
     print(trainingPaths)
-    
+
+    loop = 0
     for path in trainingPaths:
-        files = [path + f for f in os.listdir(path) if os.path.isfile(path + f) and f.endswith(".inkml")]
-        # for a specific file in that folder
-        # files = [path + 'MfrDB0033.inkml']
-        for inkml_file in files:
-            print(inkml_file)
+        while (loop < 1):
+            files = [path + f for f in os.listdir(path) if os.path.isfile(path + f) and f.endswith(".inkml")]
+            # for a specific file in that folder
+            # files = [path + 'MfrDB0033.inkml']
+            for inkml_file in files:
+                print('Start processing :', inkml_file)
+                
+                basename = inkml_file[inkml_file.rfind('/') + 1:inkml_file.rfind('.')]
+                path = inkml_file[:inkml_file.rfind('/') + 1]
+                lg_file = path + 'lg/' + basename + '.lg'        
+                csv_file = path + 'csv/' + basename + '.csv'
             
-            basename = inkml_file[inkml_file.rfind('/') + 1:inkml_file.rfind('.')]
-            path = inkml_file[:inkml_file.rfind('/') + 1]
-            lg_file = path + 'lg/' + basename + '.lg'        
-            csv_file = path + 'csv/' + basename + '.csv'
-            
-            if not os.path.exists(csv_file):
-                parser.convertStrokesToCsv(inkml_file)
-            if not os.path.exists(lg_file):
-                parser.convertInkmlToLg(inkml_file)
-            symbolList, labelList = getStrokeIds(lg_file)
-            featureExtraction(inkml_file)
+                if not os.path.exists(csv_file):
+                    parser.convertStrokesToCsv(inkml_file)
+                if not os.path.exists(lg_file):
+                    parser.convertInkmlToLg(inkml_file)
+                symbolList, labelList = getStrokeIds(lg_file)
+                featureExtraction(inkml_file)
+                loop += 1
+                print ("Done Processing :", inkml_file)
         #pdb.set_trace()
     return featureData, labelData
 
--- normalize.py
+++ normalize.py
@@ -4,8 +4,11 @@ import scipy.ndimage
 from scipy import misc
 import pdb
 from PIL import Image
+import random
 
 RESAMPLE_POINTS = 30
+
+debugCount = 0
 
 def readFile(fileName):
     inputFile = open(fileName, 'rU', newline = '\n')
@@ -13,10 +16,22 @@ def readFile(fileName):
     coordinates = {}
     for line in csvReader:
         coordinates[line[0]] = []
-        for i in range(len(line) - 2):
+        for i in range(len(line) - 1):
             lineList = line[i + 1].split(" ")
             coordinates[line[0]].append([float(lineList[0]), float(lineList[1])])
     return coordinates
+
+
+def duplicatePointRemoval(inputPoints):
+    containerMap = {}
+    outputPoints = []
+    for point in inputPoints:
+        key = str(point[0]) + str(point[1])
+        if key not in containerMap:
+            outputPoints.append(point)
+        containerMap[key] = 1
+    return outputPoints
+
 
             
 def widthNormalizeStroke(coordinates, strokeList):
@@ -27,13 +42,16 @@ def widthNormalizeStroke(coordinates, strokeList):
         for coord in coordinates[id]:
             xCord.append(coord[0])
             yCord.append(coord[1])    
-
     yMin = min(yCord)
     yMax = max(yCord)
     xMin = min(xCord)
     xMax = max(xCord)
     maxYDiff = yMax - yMin
     maxXDiff = xMax - xMin
+
+    if maxYDiff == 0:
+        pdb.set_trace()
+        
     aspectRatio = (xMax - xMin)/float((yMax - yMin))
     expectedYValue = 1
     expectedXValue = aspectRatio * expectedYValue
@@ -47,8 +65,8 @@ def widthNormalizeStroke(coordinates, strokeList):
             expectedYValue / maxYDiff
             newCoordinates[key].append([newX, newY])
 
-    if (strokeList[0] == '7'):
-        pdb.set_trace()
+    # if (strokeList[0] == '7'):
+    #     pdb.set_trace()
 
     # Create an image file
     #pdb.set_trace()
@@ -75,44 +93,86 @@ def smoothing(fileName):
 def resampleSymbol(coordinates, strokeList):
     numberElements = len(strokeList)
     newCoordinates = {}
+    lengthMap = {}
+    totalPoints = 0.0
     for stroke in strokeList:
         newCoordinates[stroke] = coordinates[stroke]
-    resamplePointList = getResampleNumbers(numberElements)
-    resampleCoordinates = resamplePoints(newCoordinates, resamplePointList)
+        lengthMap[stroke] = len(coordinates[stroke])
+        totalPoints += len(coordinates[stroke])
+    resamplePointMap = getResampleNumbers(numberElements, lengthMap, totalPoints)
+    resampleCoordinates = resamplePoints(newCoordinates, resamplePointMap)
 
     return resampleCoordinates
 
-def getResampleNumbers(number):
+def getResampleNumbers(number, lengthMap, totalPoints):
     global RESAMPLE_POINTS
-    complete = int(RESAMPLE_POINTS/number)
-    difference = RESAMPLE_POINTS - complete * number
-    resampleList = [complete for i in range(number)]
+    concernedPoints = RESAMPLE_POINTS
+    distributedLength = {}
+    finalLength = 0
     #pdb.set_trace()
-    for i in range(difference):
-        resampleList[i] += 1
-    return resampleList
+    for key in lengthMap.keys():
+        if (lengthMap[key] == 1 or lengthMap[key] == 2):
+            distributedLength[key] = lengthMap[key]
+            concernedPoints -= lengthMap[key]
+            totalPoints -= lengthMap[key]
+        else:
+            distributedLength[key] = 0
 
-def resamplePoints(coordinates, pointsList):
+    # Update to incoporate for single points strokes symbol
+    if totalPoints != 0:
+        ratio = concernedPoints/totalPoints
+        tempMap = {}
+        for key in lengthMap.keys():
+            if (distributedLength[key] != 1 and distributedLength[key] != 2):
+                tempMap[key] = int(ratio * lengthMap[key])
+                finalLength += int(ratio * lengthMap[key])
+    #pdb.set_trace()
+        differencePoints = concernedPoints - finalLength
+        while (differencePoints != 0):
+            key = random.choice(list(tempMap.keys()))
+            tempMap[key] += 1
+            differencePoints -= 1
+        distributedLength.update(tempMap)
+    #pdb.set_trace()
+    return distributedLength
+    
+# def getResampleNumbers(number):
+#     global RESAMPLE_POINTS
+#     complete = int(RESAMPLE_POINTS/number)
+#     difference = RESAMPLE_POINTS - complete * number
+#     resampleList = [complete for i in range(number)]
+#     #pdb.set_trace()
+#     for i in range(difference):
+#         resampleList[i] += 1
+#     return resampleList
+
+def resamplePoints(coordinates, pointsMap):
+    global debugCount
     newCoordinates = {}
-    pointIndex = 0
+
     for key in coordinates.keys():
-        numberResamplePoints = pointsList[pointIndex]
-        #pdb.set_trace()
-        pointIndex += 1
+        numberResamplePoints = pointsMap[key]
+
+        if numberResamplePoints == 0:
+            pdb.set_trace()
         accStrokeLength = []
         accStrokeLength.append(0)
         for i in range(len(coordinates[key]) - 1):
             accStrokeLength.append(accStrokeLength[i] + \
                         eucledianDist(coordinates[key][i], \
                             coordinates[key][i + 1]))
-        resampleDist = int(accStrokeLength[len(accStrokeLength) - 
-        1]/numberResamplePoints)
+        #resampleDist = int(accStrokeLength[len(accStrokeLength) - 
+        #1]/numberResamplePoints)
+        resampleDist = accStrokeLength[len(accStrokeLength) - 
+        1]/numberResamplePoints
         newPointList = []
         begin = coordinates[key][0]
         newPointList.append(begin)
         #pdb.set_trace()
         j = 1
         for p in range(1, (numberResamplePoints - 1)):
+            #pdb.set_trace()
+            debugCount += 1
             while accStrokeLength[j] < (p * resampleDist):
                 j += 1
             interpolationFactor = (p * resampleDist - accStrokeLength[j - 1])/ \
@@ -124,9 +184,13 @@ def resamplePoints(coordinates, pointsList):
                                                  coordinates[key][j - 1][1]) * \
                                                  interpolationFactor
             newPointList.append([newX, newY])
+
+        
         lastElement = len(coordinates[key]) - 1
-        newPointList.append([coordinates[key][lastElement][0], \
-                              coordinates[key][lastElement][1]])
+        # Consider for list containing one element
+        if (lastElement > 0):
+            newPointList.append([coordinates[key][lastElement][0], \
+                                coordinates[key][lastElement][1]])
         newCoordinates[key] = newPointList
     #pdb.set_trace()
     return newCoordinates
