b61d9c04354a768c6ebbe4ea7b09dcd8bc802dfc
Kedarnath <fussion21@gmail.com>
2015-04-05 19:39:29 -0400

    Before removing comments

--- clean.py
+++ clean.py
@@ -1,11 +1,13 @@
-import shutil
 import os
 
-trainingFolderPath = "./TrainINKML_v3/"
-trainingPaths = [trainingFolderPath + f + "/" for f in os.listdir(trainingFolderPath) if os.path.isdir(trainingFolderPath + f)]
-for path in trainingPaths:
-    if os.path.exists(path + "csv/"):
-        shutil.rmtree(path + "csv/")
-    if os.path.exists(path + "lg/"):
-        shutil.rmtree(path + "lg/")
+trainingFolderPath = "/home/kedarnath/Desktop/NN_output/train_out_lg_NN/"
+testingFolderPath = "/home/kedarnath/Desktop/NN_output/test_out_lg_NN/"
+
+trainFiles = [f for f in os.listdir(trainingFolderPath) if os.path.isfile(trainingFolderPath + f) and f.endswith(".lg")]
+testFiles = [f for f in os.listdir(testingFolderPath) if os.path.isfile(testingFolderPath + f) and f.endswith(".lg")]
+
+print("Checking all files...")
+for f in testFiles:
+    if f in trainFiles:
+        print ("File " + f + " is in both datasets")
 
